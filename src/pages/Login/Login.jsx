eb1c539521c2da304f8374e012dc38ab3a5eca47
subas-roy <subasroy46@gmail.com>
2024-09-29 04:04:51 +0600

    added login route

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+const Login = () => {
+  return (
+    <div>
+      <Navbar/>
+      <h2>This is Login</h2>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = () => {
     <li><NavLink to={"/"}>Home</NavLink></li>
     <li><NavLink to={"/about"}>About</NavLink></li>
     <li><NavLink to={"/career"}>Career</NavLink></li>
+    <li><NavLink to={"/Login"}>Login</NavLink></li>
   </>
   return (
     <div className="navbar bg-base-100">
