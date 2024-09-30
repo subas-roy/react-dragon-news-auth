5aa88357b7e6b1626d071ee5a801d832555b10eb
subas-roy <subasroy46@gmail.com>
2024-09-30 06:49:00 +0600

    Create Private route and do not redirect on reload

--- src/pages/Home/NewsCard.jsx
+++ src/pages/Home/NewsCard.jsx
@@ -12,9 +12,11 @@ const NewsCard = ({ news }) => {
       <div className="card-body">
         <h2 className="card-title">{title}</h2>
         {
-          details.length > 200 ?
-          <p>{details.slice(0, 200)} <Link to={`/news/${_id}`} className="btn btn-link font-bold text-blue-600">Read More...</Link></p> :
-          <p>{details}</p>
+          details.length > 200
+            ?
+            <p>{details.slice(0, 200)} <Link to={`/news/${_id}`} className="btn btn-link font-bold text-blue-600">Read More...</Link></p>
+            :
+            <p>{details}</p>
         }
       </div>
     </div>
--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,8 +1,24 @@
+import { useParams } from 'react-router-dom';
+import Header from '../../pages/Shared/Header/Header';
+import Navbar from '../../pages/Shared/Navbar/Navbar';
+import RightSideNav from '../Shared/RightSideNav/RightSideNav';
 
 const News = () => {
+  const {id} = useParams();
   return (
     <div>
-      <h2>News Detail</h2>
+      <Header/>
+      <Navbar/>
+      <div className='grid grid-cols-4'>
+        <div className='col-span-3'>
+          <h2 className='text-3xl'>news detail</h2>
+          <p>{id}</p>
+        </div>
+        <div>
+          <RightSideNav/>
+        </div>
+      </div>
+      
     </div>
   );
 };
